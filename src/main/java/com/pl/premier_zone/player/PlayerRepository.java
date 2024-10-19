925c97b8cfaedcc2db2cb676745c8e9ca89c05f3
Rod97139 <fayaflame@gmail.com>
2024-10-19 20:56:31 +0200

    update Player Repository

--- src/main/java/com/pl/premier_zone/player/PlayerRepository.java
+++ src/main/java/com/pl/premier_zone/player/PlayerRepository.java
@@ -3,12 +3,14 @@ package com.pl.premier_zone.player;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PlayerRepository extends JpaRepository<Player, String> {
     void deleteByName(String playerName);
     Optional<Player> findByName(String name);
+    List<Player> findByTeam(String teamName);
 }
 
 
